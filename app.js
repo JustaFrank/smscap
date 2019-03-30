851cd2a04e12b4913e6a5c186c23776d6a670e67
Frank Zheng <ztcfrank123@gmail.com>
2019-03-30 16:12:58 -0700

    fixed signale bug

--- app.js
+++ app.js
@@ -231,7 +231,7 @@ async function removeFromWhitelist (proxyNumber, number) {
 }
 
 function sendSMS (from, to, body) {
-  this.logger.info(`Sending from ${from} to ${to}: ${body}`)
+  this.signale.info(`Sending from ${from} to ${to}: ${body}`)
   client.messages.create({
     from,
     to,
